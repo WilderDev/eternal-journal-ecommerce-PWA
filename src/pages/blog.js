41ebb99580ff5d8ad4e6da52ed5dfe20872a4a5a
William Wilder <WilliamJohnWilder@outlook.com>
2020-12-27 05:31:26 -0800

    ADD: Working Blog Page & Pages!!!

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,57 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../template/layout"
 import SEO from "../template/seo"
 
-const Blog = () => (
-  <Layout>
-    <SEO title="Blog" />
-    <h1>Blog</h1>
-    <p>Welcome to the blog page</p>
-  </Layout>
-)
+import blogPageStyles from "../styles/blogPage.module.scss"
+
+const Blog = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark
+
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <div className={blogPageStyles.container}>
+        <h1>Blog Posts</h1>
+        <div className={blogPageStyles.allPosts}>
+          {posts
+            .filter(post => post.node.frontmatter.title.length > 0)
+            .map(({ node: post }) => {
+              return (
+                <div className={blogPageStyles.postPreview} key={post.id}>
+                  <h2>
+                    <Link to={post.frontmatter.path}>
+                      {post.frontmatter.title}
+                    </Link>
+                  </h2>
+                  <h3>{post.frontmatter.date}</h3>
+                  <p>{post.excerpt}</p>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const blogQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
