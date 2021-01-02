a837213e9f6e0a2fec1c3fe16113e37eba94afdb
William Wilder <WilliamJohnWilder@outlook.com>
2021-01-02 04:56:58 -0800

    FIX: Opacity on blog Posts

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,33 @@
 import React from "react"
 
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import Layout from "../template/layout"
 import SEO from "../template/seo"
 
-const Contact = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <h1>Contact</h1>
-    <p>Welcome to the contact page</p>
-  </Layout>
-)
+const Contact = () => {
+  const { image } = useStaticQuery(
+    graphql`
+      query Images {
+        image: file(relativePath: { eq: "bg/kidsInNeed.png" }) {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <h1>Contact</h1>
+      <Img fluid={image.childImageSharp.fluid} alt="test" />
+    </Layout>
+  )
+}
 
 export default Contact
