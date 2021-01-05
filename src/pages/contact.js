f410c5981582d1d8d446ba3fb67d3bb09421b10a
William Wilder <WilliamJohnWilder@outlook.com>
2021-01-04 16:51:48 -0800

    STYLE: Mission Page (Responsive)

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,31 +1,13 @@
 import React from "react"
 
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-
 import Layout from "../template/layout"
 import SEO from "../template/seo"
 
 const Contact = () => {
-  const { image } = useStaticQuery(
-    graphql`
-      query Images {
-        image: file(relativePath: { eq: "bg/kidsInNeed.png" }) {
-          id
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `
-  )
   return (
     <Layout>
       <SEO title="Contact" />
       <h1>Contact</h1>
-      <Img fluid={image.childImageSharp.fluid} alt="test" />
     </Layout>
   )
 }
--- src/pages/mission.js
+++ src/pages/mission.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery } from "gatsby"
 
+import missionStyles from "../styles/mission.module.scss"
 import Img from "gatsby-image"
 
 import Layout from "../template/layout"
@@ -23,28 +24,34 @@ const OurMission = () => {
   return (
     <Layout>
       <SEO title="Our Mission" />
-      <h1>Our Mission</h1>
-      <h2>To Give & Grow</h2>
-      <h5>Our Mission Is Simple.</h5>
-      <p>We strive to provide Clarity to those who seek a better life.</p>
-
-      <hr />
-      <h5>Beyond Our Customers</h5>
-
-      <p>Part of Growing internally is giving back.</p>
-
-      <p>
-        That is why we are happy to give part of every sale to our friends at
-        the:{" "}
-        <a href="https://www.kinf.org/" target="_blank" rel="noreferrer">
-          Kids In Need Foundation
-        </a>
-      </p>
-      <p>
-        What better way to buy a journal, than to do it by giving one to someone
-        in need!
-      </p>
-      <Img fluid={image.childImageSharp.fluid} alt="Kids In Need" />
+      <div className={missionStyles.container}>
+        <h1>Our Mission: To Give & Grow</h1>
+        {/* <h2>To Give & Grow</h2> */}
+        <h4>Our Goal Is Simple.</h4>
+        <p>
+          We strive to provide <b>Clarity</b> for those who seek a better life.
+        </p>
+        <hr />
+        <h4>Beyond Our Customers</h4>
+        <h5>Part of Growing internally is giving back.</h5>
+        <p>
+          That is why we are happy to give part of every sale to our friends at
+          the:{" "}
+          <a href="https://www.kinf.org/" target="_blank" rel="noreferrer">
+            Kids In Need Foundation
+          </a>
+        </p>
+        <p>
+          What better way to buy a journal, than to do it by giving one to
+          someone in need!
+        </p>
+        <Img
+          className={missionStyles.imageLogo}
+          fluid={image.childImageSharp.fluid}
+          alt="Kids In Need"
+        />
+        {/* // !!TODO: Total Amount Donated Counter */}
+      </div>
     </Layout>
   )
 }
