28bbd0a5990f871815b91a1ece774a762ba216ae
William Wilder <WilliamJohnWilder@outlook.com>
2021-01-05 14:46:30 -0800

    STYLE: Podcast Page (Responsive)

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -10,23 +10,104 @@ const Podcast = () => (
     <SEO title="Podcast" />
     <div className={podcastStyles.container}>
       <div className={podcastStyles.podHeading}>
-        <h1>Podcast</h1>
-        <p>Welcome to the podcast page</p>
-      </div>
-      <div className={podcastStyles.podRecent}>
-        <h3>Most Recent Podcasts</h3>
-        <h5>Title</h5>
-        <h6>Actual Thing</h6>
+        <h1>Guided Growth Podcast</h1>
         <p>
-          Description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Accusantium, vero!
+          Welcome to the podcast page. Here you will find our Guided Growth
+          Podcast
+        </p>
+        <p>
+          <b>Topics Include:</b> Growth-Mindset, Taking Action, Building Your
+          Identity, Journaling & More!
         </p>
       </div>
+      <hr />
+      <h3>Most Recent Podcasts</h3>
+      <div className={podcastStyles.podRecent}>
+        <div className={podcastStyles.card1}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card2}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card3}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+      </div>
+      <hr />
+      <h3>All Podcasts</h3>
       <div className={podcastStyles.allCasts}>
-        <h3>All Podcasts</h3>
-        <h5>Title</h5>
-        <h6>Thingoe</h6>
-        <p>Desc</p>
+        <div className={podcastStyles.card4}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card5}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card6}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card7}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card8}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
+        <div className={podcastStyles.card9}>
+          <h5>Title</h5>
+          {/* <h6>Actual Thing</h6> */}
+          <img src="https://source.unsplash.com/random/150x150" alt="" />
+          <p>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Accusantium, vero!
+          </p>
+        </div>
         {/* !!!TODO: Add pagination */}
       </div>
     </div>
