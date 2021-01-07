ed5f237136b114d66feff4dcf1cb7e5716938dff
William Wilder <WilliamJohnWilder@outlook.com>
2021-01-06 16:11:29 -0800

    ADD: Netlify Form Submission Test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,26 +16,28 @@ const Contact = () => {
         <h4>We Would Love To Hear From You</h4>
         <div className={contactStyles.formContainer}>
           <form
+            name="contact"
             method="POST"
             netlify-honeypot="bot-field"
             data-netlify="true"
-            name="contact"
           >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <label>
               Name
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" />
             </label>
             <label>
               Email
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" />
             </label>
             <label>
               Subject
-              <input type="text" name="subject" id="subject" />
+              <input type="text" name="subject" />
             </label>
             <label>
               Message
-              <textarea name="message" id="message" rows="5" />
+              <textarea name="message" rows="5" />
             </label>
             <button type="submit">
               Send <GrSend />
